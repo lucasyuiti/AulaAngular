f0bf9b8c8c4631b8493502755bac1261cd78e2db
yuiti <lucas.yuiti@bindflow.com.br>
2023-07-26 16:53:56 -0300

    Tentando fazer um front funcional

---
+++ src/app/primeiro-diretorio/teste/teste.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+var TesteHtml = './teste.component.html';
+
+@Component({
+  selector: 'app-teste',
+  templateUrl: './teste.component.html',
+  template: '<p>Meu primeiro componente com Angular 2!</p>',
+  styleUrls: ['./teste.component.css']
+})
+export class TesteComponent {
+
+}
---
+++ src/app/primeiro-diretorio/teste/teste.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TesteComponent } from '../teste/teste.component';
+
+
+
+@NgModule({
+  declarations: [
+    TesteComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TesteModule { }
